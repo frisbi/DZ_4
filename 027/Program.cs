99c3be11120490c8a485dec37d99a96f81a90821
Vladimir <dr.frisbi@gmail.com>
2023-01-18 21:10:35 +0700

    add 025, 027, 029

---
+++ 027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum (int summa)
+{
+    int s = 0;
+    while (a > 0)
+    {
+        s = s + a % 10;
+        a = a / 10;
+    }
+    return s;
+}
+Console.WriteLine($"Сумма цифр в числе равна {sum(a)} ");
